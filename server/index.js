23182488eec55403cc2465812afdeebfbca66a04
Mouhamadou-Soumare <82391111+Mouhamadou-Soumare@users.noreply.github.com>
2024-01-31 10:27:00 +0100

    resolve api bug

--- server/index.js
+++ server/index.js
@@ -1,8 +1,27 @@
-import express from "express";
+// index.js
 
+import express from 'express';
+import authRoute from './src/routes/auth.js';
+import productRoute from "./src/routes/product.js";
+import connectDB from './config/db.config.js';
+import bodyParser from 'body-parser';
+import cors from 'cors'; 
+
+// serveur
 const app = express();
-const port = 3000;
 
+// Connexion à la base de données
+connectDB();
+
+// Utilisation de bodyParser pour parser le corps des requêtes en JSON
+app.use(bodyParser.json());
+app.use(cors());
+
+// Utilisation des routes
+app.use('/auth', authRoute);
+app.use('/product', productRoute); // Utilisez la route du produit
+
+// Middleware pour autoriser les requêtes CORS
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://mangi-client.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
@@ -14,6 +33,8 @@ app.use("/", (req, res) => {
     res.send("Le serveur fonctionne");
 });
 
+const port = process.env.PORT || 3000;
+
 app.listen(port, () => {
-    console.log(`Le serveur démarre sur le port ${port}`);
+    console.log(`Le serveur est démarré sur le port ${port}`);
 });
