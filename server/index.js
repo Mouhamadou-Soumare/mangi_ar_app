2f6d63ee983bc86f49fc918f2dd1f98c24ee642f
Mouhamadou Soumare <mouhamadou-soumare@hotmail.com>
2025-04-20 21:09:51 +0200

    update cors

--- server/index.js
+++ server/index.js
@@ -5,32 +5,35 @@ import authRoute from './src/routes/auth.js';
 import productRoute from "./src/routes/product.js";
 import connectDB from './config/db.config.js';
 import bodyParser from 'body-parser';
-import cors from 'cors'; 
+import cors from 'cors';
 
-// serveur
 const app = express();
 
 // Connexion à la base de données
 connectDB();
 
-// Utilisation de bodyParser pour parser le corps des requêtes en JSON
+// Middleware
+app.use(cors({
+  origin: 'https://mangi-client.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+
 app.use(bodyParser.json());
-app.use(cors());
 
-// Utilisation des routes
+// Routes
 app.use('/auth', authRoute);
-app.use('/product', productRoute); // Utilisez la route du produit
+app.use('/product', productRoute);
 
-// Middleware pour autoriser les requêtes CORS
+// Middleware CORS fallback (facultatif si cors() est bien configuré)
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://mangi-client.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', 'https://mangi-client.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
-// Exportez une fonction handler pour Vercel
+// Export pour Vercel (serverless handler)
 export default (req, res) => {
-  // Votre logique de routage habituelle
   app(req, res);
 };
