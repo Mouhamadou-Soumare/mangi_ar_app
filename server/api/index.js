3abd7df0ece2fddb66b02571cb59e70de9838968
Mouhamadou Soumare <mouhamadou-soumare@hotmail.com>
2025-04-21 19:12:29 +0200

    updt deploiement

--- server/api/index.js
+++ server/api/index.js
@@ -1,30 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import connectDB from "../models/db.config.js";
-import authRoute from "../routes/auth.js";
-import productRoute from "../routes/product.js";
+import connectDB from '../config/db.config.js';
+import authRoute from '../src/routes/auth.js';
+import productRoute from '../src/routes/product.js';
 
-// Connexion BDD
-connectDB();
+connectDB(); // Connexion DB
 
 const app = express();
 
-// CORS config - DOIT être au tout début
 const corsOptions = {
-  origin: 'https://mangi-client.vercel.app',
+  origin: ['https://mangi-client.vercel.app'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
 
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); // gestion des preflight
+app.options('*', cors(corsOptions));
 
 app.use(bodyParser.json());
 
 app.use('/auth', authRoute);
 app.use('/product', productRoute);
 
-// Export du handler pour Vercel
 export default app;
